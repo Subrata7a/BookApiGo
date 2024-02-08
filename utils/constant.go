5bc9cd4ba76c47087103cd7cfb80fd4ea69e4bf3
Subrata7a <subrata@appscode.com>
2024-02-08 13:10:17 +0600

    ckSecond

--- utils/constant.go
+++ utils/constant.go
@@ -1,1 +1,12 @@
 package utils
+
+const (
+	DEFAULT_PORT = "8080"
+)
+
+type ErrorType string
+
+const (
+	StatusBadRequest ErrorType = "StatusBadRequest"
+	StatusNotFound   ErrorType = "StatusNotFound"
+)
